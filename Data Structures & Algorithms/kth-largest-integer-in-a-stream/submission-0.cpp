4a46ab4fa8e339ff447b831ea08f2cd14ce6698b
Akash Singh <akisini1847u@gmail.com>
2026-07-12 00:25:42 +0530

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.cpp
@@ -0,0 +1,22 @@
+class KthLargest {
+public:
+    priority_queue<int,vector<int>,greater<int>> minheap;
+    int k;
+    KthLargest(int k, vector<int>& nums) {
+        this->k=k;
+        for(int x:nums){
+            minheap.push(x);
+            if(minheap.size()>k){
+                minheap.pop();
+            }
+        }
+    }
+    
+    int add(int val) {
+        minheap.push(val);
+        if(minheap.size()>k){
+            minheap.pop();
+        }
+        return minheap.top();
+    }
+};
