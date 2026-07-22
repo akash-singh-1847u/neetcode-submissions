fb3820561d1c0428a9d638925f1dbf303018aa31
Akash Singh <akisini1847u@gmail.com>
2026-07-22 15:02:21 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int n;
+    vector<vector<int>> res;
+    unordered_set<int> st;
+    void solve(vector<int> tmp, vector<int>& nums){
+        if(st.size()==n){
+            res.push_back(tmp);
+            return;
+        }
+        for(int i=0;i<n;i++){
+            if(st.find(nums[i])==st.end()){
+                tmp.push_back(nums[i]);
+                st.insert(nums[i]);
+                solve(tmp,nums);
+                tmp.pop_back();
+                st.erase(nums[i]);
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        n=nums.size();
+        vector<int> tmp;
+        solve(tmp,nums);
+        return res;
+    }
+};
