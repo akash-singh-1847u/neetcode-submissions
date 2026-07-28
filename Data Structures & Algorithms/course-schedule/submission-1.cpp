5fe63aec6f8baf3629fe1a44fc0d41fbae27b382
Akash Singh <akisini1847u@gmail.com>
2026-07-28 15:26:54 +0530

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    queue<int> qu;
+    bool topological_sort(map<int,vector<int>>& mp,vector<int>& indegree,int numCourses){
+        int n=0;
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0){
+                qu.push(i);
+            }
+        }
+        while(!qu.empty()){
+            int u=qu.front();
+            qu.pop();
+            n++;
+            for(auto v:mp[u]){
+                indegree[v]--;
+                if(indegree[v]==0){
+                    qu.push(v);
+                }
+            }
+        }
+        if(n==numCourses){
+            return true;
+        }
+        return false;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        map<int,vector<int>> mp;
+        vector<int> indegree(numCourses,0);
+        for(auto ar:prerequisites){
+            int a=ar[0];
+            int b=ar[1];
+            indegree[a]++;
+            mp[b].push_back(a);
+        }
+        return topological_sort(mp,indegree,numCourses);
+    }
+};
