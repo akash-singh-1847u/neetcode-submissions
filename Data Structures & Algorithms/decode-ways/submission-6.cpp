1aed1468cc3f83d2d8c8dcf4ad1582ac8f42b28b
Akash Singh <akisini1847u@gmail.com>
2026-08-04 21:27:37 +0530

    Add: decode-ways - submission-6

---
+++ Data Structures & Algorithms/decode-ways/submission-6.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        if(s[0]=='0'){
+            return 0;
+        }
+        vector<int> res(s.size()+1);
+        res[s.size()]=1;
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]=='0'){
+                res[i]=0;
+                continue;
+            }
+            res[i]=res[i+1];
+            if(i+1<s.size() && (s[i]=='1' || (s[i]=='2' && s[i+1]<'7'))){
+                res[i]+=res[i+2];
+            }
+        }
+        return res[0];
+    }
+};
