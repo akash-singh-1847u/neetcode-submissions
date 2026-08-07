eb2bd0f3390c3940ca62f76cf6d5ebbe8b9bdb99
Akash Singh <akisini1847u@gmail.com>
2026-08-08 00:43:01 +0530

    Add: interleaving-string - submission-4

---
+++ Data Structures & Algorithms/interleaving-string/submission-4.cpp
@@ -0,0 +1,37 @@
+
+class Solution {
+public:
+    int tg[101][101];
+    bool solve(string s1, string s2, string s3,int i,int j){
+        if(s1.size()+s2.size()!=s3.size()){
+            return false;
+        }
+        if(i==s1.size() && j==s2.size()){
+            return true;
+        }
+        if(tg[i][j]!=-1){
+            return tg[i][j];
+        }
+        bool ans=false;
+        int k=i+j;
+        if(i<s1.size() && s1[i]==s3[k]){
+            ans=ans || solve(s1,s2,s3,i+1,j);
+        }
+        if(j<s2.size() && s2[j]==s3[k]){
+            ans=ans || solve(s1,s2,s3,i,j+1);
+        }
+        tg[i][j]=ans;
+        return ans;
+    }
+    bool isInterleave(string s1, string s2, string s3) {
+        if(s1=="" && s2=="" && s3==""){
+            return true;
+        }
+        for(int i=0;i<101;i++){
+            for(int j=0;j<101;j++){
+                tg[i][j]=-1;
+            }
+        }
+        return solve(s1,s2,s3,0,0);
+    }
+};
